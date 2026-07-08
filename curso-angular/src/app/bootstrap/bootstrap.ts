4aabab2166d7e1a544298c0b863fbf720ad4483a
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-07-08 15:26:32 -0300

    bootstrap no Angular

---
+++ curso-angular/src/app/bootstrap/bootstrap.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bootstrap',
+  imports: [],
+  templateUrl: './bootstrap.html',
+  styleUrl: './bootstrap.css',
+})
+export class Bootstrap {}
