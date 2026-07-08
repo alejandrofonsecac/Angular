57088e13a78b69a9244e37c9bb9429bdb76cedd9
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-07-08 10:41:25 -0300

    interpolação de texto

--- curso-angular/src/app/primeiro-componente/primeiro-componente.ts
+++ curso-angular/src/app/primeiro-componente/primeiro-componente.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './primeiro-componente.html',
   styleUrl: './primeiro-componente.css',
 })
-export class PrimeiroComponente {}
+export class PrimeiroComponente {
+  name:string = 'Jorge';
+  media:number = 8;
+  pessoa = {
+    nome: 'Jorge',
+    idade: 18
+  }
+
+  mensagem():string{
+    return "Hello World"
+  }
+}
