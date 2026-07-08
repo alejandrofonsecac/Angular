0ae47306ab2b43f6ed2a5aa9c01e76daf66491cd
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-07-08 10:26:18 -0300

    inicial commit

---
+++ curso-angular/src/app/primeiro-componente/primeiro-componente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primeiro-componente',
+  imports: [],
+  templateUrl: './primeiro-componente.html',
+  styleUrl: './primeiro-componente.css',
+})
+export class PrimeiroComponente {}
